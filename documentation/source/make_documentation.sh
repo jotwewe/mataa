7f799745ec0e8a5d061d59e2fa833ee4c096ecb4
jww <jens_w2@gmx.net>
2020-04-15 09:48:13 +0200

    enable building docs without modified .octaverc

--- documentation/source/make_documentation.sh
+++ documentation/source/make_documentation.sh
@@ -2,7 +2,7 @@
 echo `dirname $0`
 cd `dirname $0`
 #/Applications/Octave.app/Contents/Resources/bin/octave --silent < ./make_tools_help_list.m
-octave --silent < ./make_tools_help_list.m
+octave --silent --path ../../mataa_tools ./make_tools_help_list.m
 #makeinfo --html --no-split MATAA_manual.texi
 #rm ../MATAA_manual.html
 #mv MATAA_manual.html ..
